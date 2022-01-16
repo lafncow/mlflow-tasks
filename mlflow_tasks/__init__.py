a9319765f29c8a14ac6faaa05919bef4dd22bc11
lafncow <lafncow@users.noreply.github.com>
2022-01-15 23:38:37 -0800

    Utility funcs

--- mlflow_tasks/__init__.py
+++ mlflow_tasks/__init__.py
@@ -1,2 +1,7 @@
 from .mlflow_tasks import Task
 from .mlflow_tasks import Flow
+
+from .mlflow_tasks import get_or_create_experiment
+from .mlflow_tasks import active_task
+from .mlflow_tasks import get_task
+from .mlflow_tasks import start_task
